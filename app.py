1992e3c2b0b43511139976944463e4f2653bc3f1
Affan-Amir <95192438+Affan-Amir@users.noreply.github.com>
2023-01-14 22:56:06 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,15 @@
 from flask import Flask, jsonify, request, render_template
+import final
+
 app = Flask(__name__, template_folder='template')
-
-@app.route('/')
-def home_page():
-    example_embed = 'This string is from python'
-    return render_template('index.html', embed=example_embed)
-
 @app.route('/test', methods=['GET', 'POST'])
 def testfn():
     # GET request
-    if request.method == 'GET':
-        message = {'greeting':7827323.77}
-        return jsonify(message)  # serialize and use JSON headers
-    # POST request
-    if request.method == 'POST':
-        print(request.get_json())  # parse as JSON
-        return 'Sucesss', 200
+   if request.method == 'GET':
+      message = {'btc_price':final.bitcoin(), 'eth_price':final.eth()}
+      return render_template('index.html', jsonify(message))
+    
 
-app.run(debug=True)
+
+if __name__ == "__main__":
+   app.run()
